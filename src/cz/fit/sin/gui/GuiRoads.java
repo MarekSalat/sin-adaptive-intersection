0b3f25ede95c6048aa4c91bc50152d35b70a0806
voneskuba <vones.kuba@gmail.com>
2014-11-25 15:47:03 +0100

    Gui - okno, oprava parametru

--- src/cz/fit/sin/gui/GuiRoads.java
+++ src/cz/fit/sin/gui/GuiRoads.java
@@ -1,19 +1,37 @@
 package cz.fit.sin.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
+
+import cz.fit.sin.agents.ExecutorAgent;
 
 public class GuiRoads extends JFrame{
 	private static final long serialVersionUID = 7563499771061984518L;
 
 	// ---
 	private GuiRoads gui;
+	private ExecutorAgent mainAgent;
+	
+	// --- Bar
+	private JMenuBar  jmenubar;
+	private JMenuItem bar_help;
+	private JMenuItem bar_about;
+	
+	// --- Listeners
+	private ListenerAbout list_bar_about;
 
-	public GuiRoads() {
+	public GuiRoads(final ExecutorAgent mainAgent) {
 		super();
 		
 		// ---
-		this.gui = this;
+		this.gui 		= this;
+		this.mainAgent 	= mainAgent;
 		
 		// ---
 		this.setTitle("SIN - Adaptive intersection");
@@ -21,29 +39,50 @@ public class GuiRoads extends JFrame{
 		this.setLocationRelativeTo(null); 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// ---d
-		this.initMenu();
+		// ---
+		this._initMenu();
 		
 		// ---
 		this._setLayout();
 		this.setVisible(true);
 		
 		// ---
-		this.InitDefaultComponents();
+		this._initDefaultComponents();
 	}
 	
 	private void _setLayout(){
 		
 	}
 	
-	private void initMenu(){
+	private void _initMenu(){
+		this.jmenubar = new JMenuBar();
+		
+		this.bar_help = new JMenu("Help");
+		
+		this.bar_about  	= new JMenuItem("About");
+		this.list_bar_about = new ListenerAbout();
+		this.bar_about.addActionListener(this.list_bar_about);
+		this.bar_help.add(this.bar_about);
+		
+		// add to bar
+		this.jmenubar.add(this.bar_help);
+
+		this.setJMenuBar(this.jmenubar);
+	}
+	
+	private void _initDefaultComponents(){
 		
 	}
 	
-	private void InitDefaultComponents(){
-		
-	}
+	/* === Classes for listeners === */
 	
+	public class ListenerAbout implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			
+			// okno pro help
+	        
+		}
+	}
 	
 	/**=========== MAIN
 	 * @param args
@@ -53,7 +92,7 @@ public class GuiRoads extends JFrame{
 		SwingUtilities.invokeLater(new Runnable(){
 			
 			public void run(){
-				GuiRoads w = new GuiRoads();
+				GuiRoads w = new GuiRoads(null);
 				w.setVisible(true);
 			}
 			
