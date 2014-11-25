fe2c83595d58693d94cf2d3d4a004a89320361e7
voneskuba <vones.kuba@gmail.com>
2014-11-25 14:52:36 +0100

    Gui - okno

--- src/cz/fit/sin/gui/GuiRoads.java
+++ src/cz/fit/sin/gui/GuiRoads.java
@@ -1,13 +1,63 @@
 package cz.fit.sin.gui;
 
-public class GuiRoads {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-	/**
+public class GuiRoads extends JFrame{
+	private static final long serialVersionUID = 7563499771061984518L;
+
+	// ---
+	private GuiRoads gui;
+
+	public GuiRoads() {
+		super();
+		
+		// ---
+		this.gui = this;
+		
+		// ---
+		this.setTitle("SIN - Adaptive intersection");
+		this.setSize(700, 400);
+		this.setLocationRelativeTo(null); 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// ---
+		this.initMenu();
+		
+		// ---
+		this._setLayout();
+		this.setVisible(true);
+		
+		// ---
+		this.InitDefaultComponents();
+	}
+	
+	private void _setLayout(){
+		
+	}
+	
+	private void initMenu(){
+		
+	}
+	
+	private void InitDefaultComponents(){
+		
+	}
+	
+	
+	/**=========== MAIN
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SwingUtilities.invokeLater(new Runnable(){
+			
+			public void run(){
+				GuiRoads w = new GuiRoads();
+				w.setVisible(true);
+			}
+			
+		});
 	}
 
 }
